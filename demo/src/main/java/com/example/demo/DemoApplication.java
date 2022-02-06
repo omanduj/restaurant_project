f51e8bd9cea1bb6d0117410a759dac93cfe71359
omanduj <omanduj@gmail.com>
2022-02-06 13:25:30 -0600

    Adds endpoint

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// @GetMapping
+	// public String hello(){
+	// 	return "Hello World";
+	// }
+
+	@GetMapping
+	public List<String> hello() {
+		// return List.of("Hello", "World");
+		return List.of( "id": 1L, "name": "bob", 
+					"email": "bob@gmail.com", "password": "123"
+				);
+	}
 }
---
+++ demo/src/main/java/com/example/demo/customer/Customer.java
@@ -0,0 +1,67 @@
+package com.example.demo.customer;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Customer(){
+    }
+
+    public Customer(Long id, String name, String email, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Customer(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password ) {
+        this.password = password;
+    }
+
+
+    @Override 
+    public String toString() {
+        return "customer{" + 
+                "id=" + id + 
+                ", name= " + name + "\'" + 
+                ", email=" + email + "\'" + 
+                ", password=" + password + 
+                "}";
+    }   
+}
